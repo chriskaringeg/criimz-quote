9afa5590987d28fcad8d4b87460cdc06a3978a73
chriskaringeg <ckaringe@gmail.com>
2019-01-15 13:53:15 +0300

    updated my app and added READme

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1,'Wars come and go but my soldiers stay in turn','Tupac Amaru Shakur'),
-    new Quote(2,'The grass is greener when you water it','Neil Barringham'),
-    new Quote(3,'The brain is wider than the sky','Emily Dickinson'),
-    new Quote(4, 'There is nothing permanent except change','Heraclitos'),
-    new Quote(5, 'You cannot shake hands with a clenched fist','Indira Gandhi'),
-    new Quote(6, 'Learn to say no to the good so you can say yes to the best','John C'),
+    new Quote(1,'Wars come and go but my soldiers stay in turn','Tupac Amaru Shakur', new Date(1993, 4, 3)),
+    new Quote(2,'The grass is greener when you water it','Neil Barringham', new Date(1993, 4, 3)),
+    new Quote(3,'The brain is wider than the sky','Emily Dickinson', new Date(1993, 4, 3)),
+    new Quote(4, 'There is nothing permanent except change','Heraclitos', new Date(1993, 4, 3)),
+    new Quote(5, 'You cannot shake hands with a clenched fist','Indira Gandhi', new Date(1993, 4, 3)),
+    new Quote(6, 'Learn to say no to the good so you can say yes to the best','John C', new Date(1993, 4, 3)),
  ]
   toogleDetails(index){
   this.quotes[index].showDescription = !this.quotes[index].showDescription;
